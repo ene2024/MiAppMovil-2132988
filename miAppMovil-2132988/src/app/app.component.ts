2f8b581bd6e926db64a51615851d8aef4991fe8f
RebecaJara <rebeca.jaramilloc@uanl.edu.mx>
2024-03-23 22:11:58 -0600

    Ciclo de vida de un componente

--- miAppMovil-2132988/src/app/app.component.ts
+++ miAppMovil-2132988/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, /*NgModule*/ } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+
+/*import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';*/
 
 @Component({
   selector: 'app-root',
@@ -10,3 +12,12 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 export class AppComponent {
   constructor() {}
 }
+/*
+@NgModule({
+  declarations: [
+    ListaAlumnosComponent],
+  imports: [],
+  providers: [],
+  bootstrap: []
+})export class AppModule { }*/
+
--- miAppMovil-2132988/src/app/home/home.page.ts
+++ miAppMovil-2132988/src/app/home/home.page.ts
@@ -1,23 +1,46 @@
 import { Component} from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/angular/standalone';
 
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, CommonModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    console.log("AppComponent: Constructor");
+  }
+  ngOnChanges() {
+    console.log("AppComponent: OnChanges");
+  }
+  ngOnInit() {
+    console.log("AppComponent: OnInit");
+  }
+  ngDoCheck() {
+    console.log("AppComponent: DoCheck");
+  }
+  ngAfterContentInit() {
+    console.log("AppComponent: AfterContentInit");
+  }
+  ngAfterContentChecked() {
+    console.log("AppComponent: AfterContentChecked");
+  }
+  ngAfterViewInit() {
+    console.log("AppComponent: AfterViewInit");
+  }
+  ngAfterViewChecked() {
+    console.log("AppComponent: AfterViewChecked");
+  }
+  ngOnDestroy() {
+    console.log("AppComponent: OnDestroy");
+  }
+
 
   titulo: string='Mi nueva página!';
 
-  hazmeClick(): void{
-    alert("Hiciste click!");
-  }
-
-  imgUrl: string = 'https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/03/Nueva-serie-de-television-de-Patricio-Estrella.jpg';
 }
