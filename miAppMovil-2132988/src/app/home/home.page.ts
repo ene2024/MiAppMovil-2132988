edd22a2f90f8aa4006769da00ead051ebcf593f0
RebecaJara <rebeca.jaramilloc@uanl.edu.mx>
2024-03-23 18:03:09 -0600

    Ejercicios Directivas de Angular

--- miAppMovil-2132988/src/app/home/home.page.ts
+++ miAppMovil-2132988/src/app/home/home.page.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, CommonModule],
 })
-export class HomePage {
+export class HomePage implements OnInit{
   constructor() {}
+
+  ngOnInit() {}
+
+  verdadero: boolean = true;
+
+  titulo: string='Mi nueva página!';
+
+  hazmeClic(): void{
+    alert("Hiciste clic!");
+  }
 }
