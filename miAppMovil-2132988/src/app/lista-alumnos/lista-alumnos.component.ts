450b0603ac5c29db9d7066634b3af7f307380308
RebecaJara <rebeca.jaramilloc@uanl.edu.mx>
2024-04-06 22:12:51 -0600

    Practica8: Action Sheet y Sliding Options

--- miAppMovil-2132988/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miAppMovil-2132988/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,17 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonList, IonItem } from '@ionic/angular/standalone';
+import { IonList, IonItem, IonItemSliding, IonItemOptions, IonItemOption, IonLabel, 
+  IonButton, IonActionSheet
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonList, IonItem],
+  imports: [CommonModule, IonList, IonItem, IonItemSliding, IonItemOptions, IonItemOption, IonLabel, IonButton,
+    IonActionSheet],
 })
 export class ListaAlumnosComponent {
-  constructor() {}
-
   alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+
+  isActionSheetOpen = false;
+  public actionSheetButtons = [
+    {
+      text: 'Eliminar',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
 
 }
